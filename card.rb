96fa95890142b99e8956b856d107efec61949f20
Lucas Ethan <asmrit1010@gmail.com>
2021-01-09 10:31:57 +0100

    Implemented class methods

--- card.rb
+++ card.rb
@@ -1,2 +1,68 @@
 class Card
+  attr_reader :suit, :rank
+  attr_accessor :revealed
+
+  # Card Suits
+  SUITS = {
+    spade: :spade,
+    club: :club,
+    heart: :heart,
+    diamond: :diamond,
+  }.freeze
+
+  RANKS = {
+    ace: :ace,
+    two: :two,
+    three: :three,
+    four: :four,
+    five: :five,
+    six: :six,
+    seven: :seven,
+    eight: :eight,
+    nine: :nine,
+    ten: :ten,
+    king: :king,
+    queen: :queen,
+    jack: :jack,
+  }.freeze
+
+  COLORS = {
+    black: :black,
+    red: :red,
+  }.freeze
+
+
+  def initialize(suit, rank, revealed = false)
+    @suit = suit
+    @rank = rank
+    @revealed = revealed
+  end
+
+  def color
+    if suit == SUITS[:heart] || suit == SUITS[:diamond]
+      COLORS[:red]
+    else
+      COLORS[:black]
+    end
+  end
+  
+  def revealed?
+    revealed
+  end
+
+  def reveal
+    self.revealed = true
+  end
+
+  def unreveal
+    self.revealed = false
+  end
+
+  def toggle_reveal
+    if revealed?
+      unreveal
+    else
+      reveal
+    end
+  end
 end
